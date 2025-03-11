2f5b33cf1d341786d5caf725dfc10a288b959275
mariaedk <mariaedkru@gmail.com>
2025-03-10 22:15:31 -0300

    ajustes na aplicação

---
+++ scripts/backend/app/services/medicao_service.py
@@ -0,0 +1,56 @@
+"""
+@author maria
+date: 2025-02-27
+"""
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.medicao_schema import MedicaoCreate, MedicaoResponse
+from app.repositories.medicao_repository import MedicaoRepository as medicao_repository
+from app.config import MessageLoader
+from sqlalchemy.exc import InvalidRequestError, DatabaseError
+from app.models.medicao_model import Medicao
+
+class MedicaoService:
+
+    @staticmethod
+    def criar_medicao(db: Session, medicao_schema: MedicaoCreate) -> MedicaoResponse:
+        if medicao_schema is None:
+            raise HTTPException(status_code=400, detail=MessageLoader.get("erro.parametro_nao_informado"))
+
+        medicao_dict = medicao_schema.model_dump()
+        medicao_obj = Medicao(**medicao_dict)
+
+        try:
+            medicao = medicao_repository.save(db, medicao_obj)
+        except InvalidRequestError:
+            db.rollback()
+            raise HTTPException(status_code=400, detail=MessageLoader.get("erro.requisicao_invalida"))
+        except DatabaseError:
+            db.rollback()
+            raise HTTPException(status_code=500, detail=MessageLoader.get("erro.banco"))
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=500, detail=f"Erro inesperado: {str(e)}")
+
+        return MedicaoResponse.model_validate(medicao)
+
+    @staticmethod
+    def listar_medicoes(db: Session):
+        medicoes = medicao_repository.find_all(db)
+        return [MedicaoResponse.model_validate(medicao) for medicao in medicoes]
+
+    @staticmethod
+    def listar_medicoes_paginadas(db: Session, limit: int = 10, offset: int = 0):
+        medicoes = medicao_repository.find_all_paginate(db, limit, offset)
+        return [MedicaoResponse.model_validate(medicao) for medicao in medicoes]
+
+    @staticmethod
+    def buscar_medicao(db: Session, medicao_id: int):
+        if medicao_id is None:
+            raise HTTPException(status_code=400, detail=MessageLoader.get("erro.parametro_nao_informado"))
+
+        medicao = medicao_repository.find_by_id(db, medicao_id)
+        if not medicao:
+            raise HTTPException(status_code=404, detail=MessageLoader.get("erro.medicao_nao_encontrada"))
+
+        return MedicaoResponse.model_validate(medicao)
